9318a752ec244763ef5e74d9dac621a1bbe1e73c
Kaan SEN <kaansen57@outlook.com>
2021-01-22 23:58:58 +0300

    pub-sub edited.

--- pub-sub/publisher.js
+++ pub-sub/publisher.js
@@ -3,14 +3,36 @@ const zeromq = require('zeromq');
 //Soket Türü
 const socket = new zeromq.Publisher();
 
+
 runPublisher();
+
 
 async function runPublisher() {
     await socket.bind("tcp://127.0.0.1:9000");
     console.log("Bağlantı Kuruldu");
-    process.stdin.once("data", async () => {
-        await socket.send(["topic1", "kaan şen"]); // kanal oluştur ve mesajı gönder 
-        await socket.send(["topic1", "kaan şen"]); // kanal oluştur ve mesajı gönder 
-        console.log("Data gönderildi!");
+    console.log("Mesaj Formatı =  kanalAdi:Mesajınız");
+    process.stdin.on("data",  async (data) => { //on ile habire bu process dinleniyor
+        /*console.log("data",data.toString());  buffer gelen datayı string çevir ama bunun yerine backtick yazım versiyonununda
+        bu sorun kalkıyor */
+          const userMessage  = data.toString().replace("\n","");
+          if(userMessage === "q"){
+            process.exit(0);
+        }
+            if(userMessage.includes(":") && userMessage.split(":").length === 2){ // splitten dönen array uzunluğu 2 olmalı [1 kanaldi, 2mesaj]  
+                 const dataSplit = userMessage.split(":");
+                    await socket.send([dataSplit[0], dataSplit[1]]); // kanal oluştur ve mesajı gönder 
+                   console.log("Data gönderildi!");
+            }else{
+                console.log("Yanlış Format \n Mesaj Formatı = kanalAdi:Mesajınız ");
+            }
     });
 }
+
+// async function runPublisher() {
+//     await socket.bind("tcp://127.0.0.1:9000");
+//     console.log("Bağlantı Kuruldu");
+//     process.stdin.once("data", async () => { //entera basınca 1 kere gönder
+//         await socket.send(["topic1", "kaan şen"]); // kanal oluştur ve mesajı gönder 
+//         console.log("Data gönderildi!");
+//     });
+// }
--- pub-sub/subscriber.js
+++ pub-sub/subscriber.js
@@ -1,14 +1,14 @@
 const zeromq = require('zeromq');
 
 const socket = new zeromq.Subscriber();
-
+const topicName  = process.argv[2] || "kaansen" // değer yoksa default olarak kaansen topicine bağlan.
 runSubscriber();
 
 async function runSubscriber(){
     await socket.connect("tcp://127.0.0.1:9000");
     console.log("Servise bağlanıldı ! ");
-    await socket.subscribe("topic1");
-    console.log("Kanala abone olundu !");
+    await socket.subscribe(topicName);
+    console.log(`${topicName} Kanalına abone olundu !`);
     
     for await (const[topic,message] of socket){
         console.log(`Kanal :${topic} , Mesaj : ${message}`);
